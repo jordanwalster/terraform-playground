96ddb4adc240a69a7fcd16371820a3e4e6d0f2d1
Jordan Walster <jordanwalster@gmail.com>
2019-02-08 17:19:45 +0000

    Fixed errors with running startup.sh

--- startup.sh
+++ startup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade -y
+sudo apt update -y
+
 # Install Apache2
-apt-get install apache2 -y
+sudo apt-get install apache2 -y
 # Install MySQL Server
-apt-get install -o Dpkg::Options::="--force-confold" mysql-server -y
+sudo apt-get install -o Dpkg::Options::="--force-confold" mysql-server -y
 # Start MySQL service
-systemctl start mysql
+sudo systemctl start mysql
 # Enable MySQL service on boot
-systemctl enable mysql
+sudo systemctl enable mysql
 # Install WordPress
-apt-get install wordpress
+sudo apt-get install wordpress -y
 
 #Store external IP as a variable
 hostname="$(curl ifconfig.me)"
 
+sudo a2dissite 000-default.conf
+
 # If wordpress config exists (which it wont), then create file and print configuration.
 if [ ! -e "/etc/apache2/sites-available/wordpress.conf" ]; then
-  echo "Alias /blog /usr/share/wordpress
+  echo "Alias / /usr/share/wordpress
         <Directory /usr/share/wordpress>
             Options FollowSymLinks
             AllowOverride Limit Options FileInfo
@@ -33,13 +35,13 @@ if [ ! -e "/etc/apache2/sites-available/wordpress.conf" ]; then
 fi 
 
 # Enable WordPress Site
-a2ensite wordpress
+sudo a2ensite wordpress
 # Restart Apache
-systemctl restart apache2.service
+sudo systemctl restart apache2.service
 
 # If WordPress DB Config exists (which it also wont), create the file and print contents.
 if [ ! -e "/etc/wordpress/config-$hostname.php" ]; then
-  echo "<?php
+  sudo echo "<?php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
 define('DB_PASSWORD', '52854fd6ed9a2fa158e17e959aede4ea');
@@ -50,8 +52,8 @@ fi
 
 # If temp db file exists (which also definitely wont), create the file and print the contents.
 if [ ! -e "/etc/wordpress/wordpress.sql" ]; then
-  echo "CREATE DATABASE wordpress; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY '52854fd6ed9a2fa158e17e959aede4ea'; FLUSH PRIVILEGES;" >> "/etc/wordpress/wordpress.sql"
+  sudo echo "CREATE DATABASE wordpress; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY '52854fd6ed9a2fa158e17e959aede4ea'; FLUSH PRIVILEGES;" >> "/etc/wordpress/wordpress.sql"
 fi
 
-cat /etc/wordpress/wordpress.sql | mysql --defaults-extra-file=/etc/mysql/debian.cnf
-systemctl restart mysql.service
+sudo cat /etc/wordpress/wordpress.sql | mysql --defaults-extra-file=/etc/mysql/debian.cnf
+sudo systemctl restart mysql.service
