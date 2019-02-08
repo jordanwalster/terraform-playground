11b9f5ec91ef86548fda21f2b278fe87b48aa925
Jordan Walster <jordanwalster@gmail.com>
2019-02-08 09:53:10 +0000

    Changed region to a variable

--- main.tf
+++ main.tf
@@ -3,16 +3,20 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_key_path" {}
+variable "region" {
+  default = "eu-west-2"
+}
 variable "key_pair" {
   default = "aws-testing"
 }
+variable "security_group_id" {}
 
 # Provider
 
 provider "aws" {
-  access_key    = "${var.aws_access_key}"
-  secret_key    = "${var.aws_secret_key}"
-  region        = "eu-west-2"
+  access_key            = "${var.aws_access_key}"
+  secret_key            = "${var.aws_secret_key}"
+  region                = "${var.region}"
 }
 
 resource "aws_instance" "apache" {
@@ -32,6 +36,10 @@ resource "aws_instance" "apache" {
             "sudo service apache2 start"
         ]
     }
+}
+
+data "aws_security_group" "selected" {
+  id = "${var.security_group_id}"
 }
 
 # Output
